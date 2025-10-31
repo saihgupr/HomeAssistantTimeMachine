bb4f81fe91fc94433a0b9318376b89ede5b06251
saihgupr <121105543+saihgupr@users.noreply.github.com>
2025-10-31 09:16:15 -0400

    Update

--- homeassistant-time-machine/run.sh
+++ homeassistant-time-machine/run.sh
@@ -4,7 +4,7 @@ export HOST="${HOST:-0.0.0.0}"
 export PORT="${PORT:-54000}"
 
 echo "======================================"
-echo "Home Assistant Time Machine v2.0.1"
+echo "Home Assistant Time Machine v2.0.1.1"
 echo "======================================"
 echo "Starting server..."
 echo "======================================"
