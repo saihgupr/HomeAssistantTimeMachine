4bcfa5990d0ee1a451164b7701d691f8b94a5b83
saihgupr <121105543+saihgupr@users.noreply.github.com>
2025-10-31 08:34:32 -0400

    Update

---
+++ homeassistant-time-machine/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export NODE_ENV="${NODE_ENV:-production}"
+export HOST="${HOST:-0.0.0.0}"
+export PORT="${PORT:-54000}"
+
+echo "======================================"
+echo "Home Assistant Time Machine v2.0.1"
+echo "======================================"
+echo "Starting server..."
+echo "======================================"
+
+node app.js
